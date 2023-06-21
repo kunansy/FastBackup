79f9ac905582af6629f0744e322f0a14293f6c54
kunansy <kolobov.kirill@list.ru>
2023-06-21 12:18:46 +0300

    add Errors::DumpError variant

--- src/lib.rs
+++ src/lib.rs
@@ -92,6 +92,8 @@ pub mod errors {
     pub enum Errors {
         #[error("Could not init with env: {0}")]
         EnvError(String),
+        #[error("Could not dump the database")]
+        DumpError(String)
     }
 
     impl From<std::env::VarError> for Errors {
