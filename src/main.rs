22afad2729682ece13556249e1edcffaaf90b806
kunansy <kolobov.kirill@list.ru>
2023-06-21 19:17:47 +0300

    implement backupper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+use backupper::{settings::Settings, logger, errors::Errors, backup, google_drive::GoogleDrive};
+use backupper::sender::Storage;
+
+#[tokio::main]
+async fn main() -> Result<(), Errors> {
+    // backup::assert_programs_exist();
+
+    Settings::load_env();
+    logger::init();
+
+    let cfg = Settings::parse()?;
+
+    let filename = backup::dump(&cfg)?;
+    let filename = Path::new(&filename);
+    let drive = GoogleDrive::new(&cfg.drive_creds);
+    drive.send(&filename).await?;
+
+    Ok(())
 }
