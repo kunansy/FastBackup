ffbbe901e33864aef8a8968acaffe626be048faf
kunansy <kolobov.kirill@list.ru>
2023-07-14 14:30:32 +0300

    use parallel tasks to dump db and init drive

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{path::Path, sync::Arc};
+use std::path::Path;
 use std::time;
 
-use backuper::{db, google_drive::DriveAuth, logger, Res, settings::Settings};
-use backuper::google_drive::GoogleDrive;
+use backuper::{db, logger, Res, settings::Settings};
 
 #[tokio::main]
 async fn main() -> Res<()> {
@@ -14,37 +13,19 @@ async fn main() -> Res<()> {
 
     let cfg = Settings::parse()?;
 
+    let drive_hdl = tokio::spawn(async move {
+        // TODO: cache folder_id
+        db::prepare_drive(&cfg.drive_creds, &cfg.drive_folder_id).await
+    });
     let dump_hdl = tokio::spawn( async {
         let c = Settings::parse().unwrap();
-        log::info!("Prepare dump");
-        let d = prepare_dump(&c).await;
-        log::info!("Dump prepared");
-        d
-    });
-    let drive_hdl = tokio::spawn(async move {
-        log::info!("Prepare drive");
-        let d = prepare_drive(&cfg.drive_creds).await;
-        log::info!("Drive prepared");
-        d
+        db::prepare_dump(&c, &c.data_folder, c.comp_level).await
     });
 
-    let dp = dump_hdl.await??;
+    let path = dump_hdl.await??;
     let (drive, folder_id) = drive_hdl.await??;
 
-    // let pool = db::init_pool(&cfg).await?;
-    // let arc_pool = Arc::new(pool);
-    //
-    // let filename = db::dump(arc_pool, &cfg.data_folder, cfg.comp_level).await?;
-    let filename = Path::new(&dp);
-    //
-    // // TODO: add two parallel threads:
-    // //  1. Init pool and dump db;
-    // //  2. Init drive, get "tracker" folder id, last dump id
-    // let drive = {
-    //     let mut d = DriveAuth::new(&cfg.drive_creds);
-    //     d.build_auth().await?;
-    //     d.build_hub()
-    // };
+    let filename = Path::new(&path);
 
     backuper::send(&drive, &filename, Some(folder_id)).await?;
 
