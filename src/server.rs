5374153c62b0a064b1900a049f28f0138e359226
kunansy <kolobov.kirill@list.ru>
2023-07-14 14:29:51 +0300

    use parallel tasks to dump db and init drive

--- src/server.rs
+++ src/server.rs
@@ -7,8 +7,7 @@ use tonic::{Request, Response, Status, transport::Server};
 
 use backup::{BackupReply, BackupRequest, RestoreRequest};
 use backup::google_drive_server::{GoogleDrive, GoogleDriveServer};
-use backuper::{db, DbConfig};
-use backuper::{google_drive::DriveAuth, settings::Settings};
+use backuper::{db, DbConfig, settings::Settings};
 
 pub mod backup {
     tonic::include_proto!("backup");
@@ -77,26 +76,23 @@ impl GoogleDrive for Backup {
             Some(cfg) => cfg
         };
 
-        let params = request.into_inner();
+        let dump_hdl = tokio::spawn(async {
+            let cfg = unsafe {CFG.clone()}.unwrap();
+            let params = request.into_inner();
 
-        let pool = db::init_pool(&params).await
-            .map_err(|e| {
-                let msg = format!("Could not create DB pool: {}", e.to_string());
-                Status::internal(&msg)
-            })?;
+            db::prepare_dump(&params, &cfg.data_folder, cfg.comp_level).await
+        });
+        let drive_hdl = tokio::spawn(async move {
+            // TODO: cache folder_id
+            db::prepare_drive(&cfg.drive_creds, &cfg.drive_folder_id).await
+        });
 
-        let arc_pool = Arc::new(pool);
-        let path = db::dump(arc_pool.clone(), &cfg.data_folder, cfg.comp_level)
-            .await
-            .map_err(|e| Status::internal(e.to_string()))?;
+        // TODO: manage errors
+        let path = dump_hdl.await.unwrap().unwrap();
+        let (drive, folder_id) = drive_hdl.await.unwrap().unwrap();
         let path = Path::new(&path);
 
-        let drive = {
-            let mut d = DriveAuth::new(&cfg.drive_creds);
-            d.build_auth().await?;
-            d.build_hub()
-        };
-        let file_id = backuper::send(&drive, path, None)
+        let file_id = backuper::send(&drive, path, Some(folder_id))
             .await
             .map_err(|e| Status::aborted(e.to_string()))?;
 
